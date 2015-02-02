957045822da81292102c252f737add9a71e3b80b
Caio Ribeiro Pereira <caio.pereira@concretesolutions.com.br>
2015-02-02 16:25:35 +0000

    added spider ads header banner

--- public/assets/js/_templates.js
+++ public/assets/js/_templates.js
@@ -51,7 +51,7 @@ function program1(depth0,data) {
     + "</strong> books.\n    </p>\n  </div>\n</div>\n<div class=\"row-fluid\">\n  ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.platforms), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</div>\n<div class=\"row-fluid\">\n  <div class=\"col-xs-12\">\n    <p class=\"center\">\n      <strong>See also <a href=\"http://devfreecasts.org?utm_source=devfreebooks&utm_medium=medium&utm_campaign=DevFreeBooks\" target=\"_blank\" rel=\"me\" title=\"a huge collection of screencasts for devs.\">DevFreeCasts</a>.</strong>\n    </p>\n  </div>\n</div>";
+  buffer += "\n</div>\n<div class=\"row-fluid\">\n  <div class=\"col-xs-12\">\n    <p class=\"center\">\n      <strong>See also <a href=\"http://devfreecasts.org?utm_source=devfreebooks&utm_medium=medium&utm_campaign=DevFreeBooks\" target=\"_blank\" rel=\"me\" title=\"a huge collection of free screencasts for devs.\">DevFreeCasts</a>.</strong>\n    </p>\n  </div>\n</div>";
   return buffer;
   });
 
